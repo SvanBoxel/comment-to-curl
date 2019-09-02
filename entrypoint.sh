63f2a569eaf54e9ace028dea0b14ee4357a2ecdc
Sebass van Boxel <svboxel@gmail.com>
2019-09-02 15:52:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,32 @@ set -u
 
 cd $GITHUB_WORKSPACE
 
+action=$(cat $GITHUB_EVENT_PATH | jq .action)
+
+if [[ $action != "created" ]] 
+then
+  exit 1
+fi 
+
+echo "Action: ${action}"
 command=$(cat $GITHUB_EVENT_PATH | jq .comment.body)
+
 if [[ ${command:0:1} == "." ]] 
 then
   exit 1
 fi
 
-echo $command 
+
+echo "Command: ${command}"
 
 manifest=$(cat .comment-to-curl.json)
-
 url=$(jq -n "$manifest" | jq -r .${command}.url)
-echo $url
+
+if [ -z "$url" ]
+then
+  exit 1
+fi
+
+echo "URL: ${url}"
 
 curl --silent "$url" > /dev/null 
