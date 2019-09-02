8e008fe3e3113e0d0ae2ad7380678efd28853538
Sebass van Boxel <svboxel@gmail.com>
2019-09-02 15:48:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,15 @@ set -u
 
 cd $GITHUB_WORKSPACE
 
-manifest=$(cat .comment-to-curl.json)
-command=$(cat $GITHUB_EVENT_PATH | jq .body)
-cat $GITHUB_EVENT_PATH
+command=$(cat $GITHUB_EVENT_PATH | jq .comment.body)
+if [[ ${command:0:1} == "." ]] 
+then
+  exit 1
+fi
+
 echo $command 
+
+manifest=$(cat .comment-to-curl.json)
 
 url=$(jq -n "$manifest" | jq -r .${command}.url)
 echo $url
